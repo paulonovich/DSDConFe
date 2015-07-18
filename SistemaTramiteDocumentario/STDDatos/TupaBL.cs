1d567742c94fc572afb0996a3dcc951922b85b7f
Guillermo <kenyi348@gmail.com>
2015-07-18 03:25:23 -0500

    Cambio 3

--- SistemaTramiteDocumentario/STDDatos/TupaBL.cs
+++ SistemaTramiteDocumentario/STDDatos/TupaBL.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace STDDatos
+{
+    public class TupaBl
+    {
+        public List<Tupa> ObtenerTupa(int codigoTramite)
+        {
+            try
+            {
+                BDDOCUMENTUMEntities datos = new BaseDAO().conexion();
+                var vResult = datos.Tupa.Where(t => t.codigoTramite == codigoTramite).ToList();
+                return vResult;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- SistemaTramiteDocumentario/STDServices/ITramite.cs
+++ SistemaTramiteDocumentario/STDServices/ITramite.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using STDDatos;
+
+namespace STDServices
+{
+    [ServiceContract]
+    public interface ITramite
+    {
+        [OperationContract]
+        List<Tramite> ListarTramites();
+    }
+}
